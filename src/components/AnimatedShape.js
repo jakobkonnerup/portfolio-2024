6e6dad9aecaec57b1658068354fafb784d78dfea
jakobkonnerup <144614107+jakobkonnerup@users.noreply.github.com>
2024-09-20 00:01:50 +0200

    Create AnimatedShape.js

---
+++ src/components/AnimatedShape.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const AnimatedShape = () => {
+  return (
+    <div className="animation-container">
+      <div className="shape1"></div>
+      <div className="shape2"></div>
+      <div className="shape3"></div>
+      <div className="shape4"></div>
+      <div className="shape5"></div>
+    </div>
+    
+    
+  );
+};
+
+export default AnimatedShape;
